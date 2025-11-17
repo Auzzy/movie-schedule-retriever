adbd87f116fb423327aa812e9fe319b443345040
Austin Noto-Moniz <mathfreak65@gmail.com>
2025-11-17 08:15:21 -0500

    Make email sender conigurable by envvar.

--- email-movie-times.py
+++ email-movie-times.py
@@ -5,13 +5,12 @@ import os
 from mailtrap import Address, Attachment, Mail, MailtrapClient
 
 from fandango_json import load_schedules_by_day
-from schedule import Filter, FullSchedule, date_range_str_parser
+from schedule import THEATER_CODE_DICT, Filter, FullSchedule, date_range_str_parser
 
 
-THEATERS = {"AMC Methuen", "AMC Boston Common"}
+def send_email(theaters_to_schedule, subject):
+    sender_name = os.getenv("MAILTRAP SENDER", "Test Movie Sender")
 
-
-def send_email(theaters_to_schedule):
     attachments = []
     for theater, schedule in theaters_to_schedule.items():
         attachments.append(
@@ -22,9 +21,9 @@ def send_email(theaters_to_schedule):
         )
 
     mail = Mail(
-        sender=Address(email=os.environ["MAILTRAP_SENDER"], name="Movie Schedules"),
+        sender=Address(email=os.environ["MAILTRAP_SENDER"], name=sender_name),
         to=[Address(email=os.environ["MAILTRAP_RECEIVER"])],
-        subject="Movie Schedules",
+        subject=subject,
         text="Schedules attached",
         attachments=attachments
     )
@@ -44,9 +43,17 @@ def collect_schedules(theater, date_range):
     return schedule_range.output(name_only=False, date_only=True)
 
 def main():
-    dates = date_range_str_parser("next movie week")
-    theaters_to_schedule = {theater: collect_schedules(theater, dates) for theater in THEATERS}
-    send_email(theaters_to_schedule)
+    date_range_str = os.getenv("SCHEDULE_RETRIEVER_DATES", "next movie week")
+    theaters_str = os.getenv("SCHEDULE_RETRIEVER_THEATERS")
+
+    dates = date_range_str_parser(date_range_str)
+    subject = f"Movie Schedules {dates[0].isoformat()}"
+    if dates[0] != dates[1]:
+        subject += f" to {dates[1].isoformat()}"
+    theaters = set(theaters_str.split(";")) if theaters_str else THEATER_CODE_DICT.keys()
+
+    theaters_to_schedule = {theater: collect_schedules(theater, dates) for theater in theaters}
+    send_email(theaters_to_schedule, subject)
 
 
 if __name__ == "__main__":
